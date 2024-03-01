2286e4ef373855fb19ccc1effd7cf60d6c583d30
EdwingDuvanHernandezHerrera <124322869+EdwingDuvanHernandezHerrera@users.noreply.github.com>
2024-02-29 23:08:27 -0500

    feat :lipstick: Se agrega modal y se cambian colores

--- js/app.js
+++ js/app.js
@@ -3,11 +3,19 @@ const sectionDC = document.querySelector(".dcComics");
 const mostrarMarvel = document.querySelector(".mostrarMarvel");
 const mostrarDC = document.querySelector(".mostrarDC");
 
+const tituloMarvel = document.querySelector(".tituloMarvel");
+const tituloDC = document.querySelector(".tituloDc");
+const hrMarvel = document.querySelector(".hrMarvel");
+const hrDC = document.querySelector(".hrDC");
+
 let heroesMarvel = [];
 let heroesDC = [];
-// const heroesTodos = [...heroesMarvel, ...mostrarDC];
 
 mostrarMarvel.addEventListener("click", async (e) => {
+    tituloDC.style.display = "none";
+    hrDC.style.display = "none";
+    tituloMarvel.style.display = "flex";
+    hrMarvel.style.display = "flex";     
     heroesMarvel = await cargarDatosMarvel();
     limpiarCards(sectionMarvel);
     limpiarCards(sectionDC);
@@ -15,10 +23,14 @@ mostrarMarvel.addEventListener("click", async (e) => {
 
 })
 
-mostrarDC.addEventListener("click", async (e) => {
+mostrarDC.addEventListener("click", async (e) => { 
+    tituloMarvel.style.display = "none";
+    hrMarvel.style.display = "none";  
+    tituloDC.style.display = "flex";
+    hrDC.style.display = "flex";    
     heroesDC = await cargarDatosDC();
     limpiarCards(sectionDC);
-    limpiarCards(sectionMarvel);
+    limpiarCards(sectionMarvel); 
     generarCards(sectionDC, heroesDC);
 
 })
@@ -49,6 +61,10 @@ function generarCards(sectionHeroes, heroes){
         botonCard.textContent = "Más información";
         botonCard.className = "botonVerMas";
 
+        botonCard.addEventListener("click", () => {
+            abrirModal(heroe);
+        });
+
         card.appendChild(imgCard);
         card.appendChild(tituloCard);
         card.appendChild(botonCard);
@@ -60,6 +76,12 @@ function generarCards(sectionHeroes, heroes){
 function limpiarCards(sectionCard){
     while(sectionCard.firstChild){
         sectionCard.removeChild(sectionCard.firstChild);
+    }
+}
+
+function agregarElementos(element){
+    while(element.firstChild){
+        element.removeChild(element.firstChild);
     }
 }
 
@@ -83,3 +105,40 @@ async function cargarDatosMarvel(){
 async function cargarDatosDC(){
     return await fetch("../storage/data/heroesDc.json").then(response => response.json());
 }
+
+
+// Función para abrir el modal con la información específica de la tarjeta
+function abrirModal(heroe) {
+    const modal = document.getElementById("myModal");
+    const modalImage = document.getElementById("modalImage");
+    const modalNombre = document.querySelector(".nombre");
+    const modalDescripcion = document.querySelector(".descripcion");
+    const modalFecha = document.querySelector(".fecha");
+
+    // Configura la imagen y el texto en el modal
+    modalImage.src = heroe.imagen;
+    modalNombre.textContent = `Nombre: ${heroe.nombre}`;
+    modalDescripcion.textContent = `Descripción: ${heroe.descrip}`;
+    modalFecha.textContent = `Fecha creación: ${heroe.fecha}`;
+
+    // Muestra el modal
+    modal.showModal();
+
+    // Agrega un evento clic al botón de cierre del modal
+    const closeButton = document.querySelector(".close");
+    closeButton.addEventListener("click", () => {
+        modal.close();
+    });
+
+    // Cierra el modal si se hace clic fuera de él
+    window.addEventListener("click", (event) => {
+        if (event.target == modal) {
+            modal.close();
+        }
+    });
+}
+
+
+
+
+
